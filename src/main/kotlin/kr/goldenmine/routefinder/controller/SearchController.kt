5a3b85116f3ffa3d7b3b2e3889c720e9599c115d
GoldenMine0502 <ehe123@naver.com>
2024-05-11 20:49:24 +0900

    어노테이션 추가

--- src/main/kotlin/kr/goldenmine/routefinder/controller/SearchController.kt
+++ src/main/kotlin/kr/goldenmine/routefinder/controller/SearchController.kt
@@ -1,4 +1,10 @@
 package kr.goldenmine.routefinder.controller
 
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/search")
 class SearchController {
+
 }
